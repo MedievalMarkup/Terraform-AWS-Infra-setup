44180c4501c94b6e15b3e34e04c344da0c076251
Adil Nehal <adil.nehal@ai.local>
2024-01-05 11:46:09 +0500

    work for Application Load Balancer

--- standarized-ALB/c10-03-alb.tf
+++ standarized-ALB/c10-03-alb.tf
@@ -1,44 +1,102 @@
-module "elb" {
-  source  = "terraform-aws-modules/elb/aws"
-  version = "4.0.1"
+# https://github.com/terraform-aws-modules/terraform-aws-alb/issues/316 
+module "alb" {
 
-  name = "${local.name}-elb"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.4.0"
 
-  subnets         = [for id in module.vpc.public_subnets: id]
+  name    = "${local.name}-alb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+  load_balancer_type = "application"
   security_groups = [module.sg_loadbalancer.security_group_id]
-  # internal        = false
 
-  listener = [
-    {
-      instance_port     = 80
-      instance_protocol = "HTTP"
-      lb_port           = 80
-      lb_protocol       = "HTTP"
-    },
-    {
-      instance_port     = 80
-      instance_protocol = "HTTP"
-      lb_port           = 81
-      lb_protocol       = "HTTP"
-      # ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
-    },
-  ]
-
-  health_check = {
-    target              = "HTTP:80/"
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-  }
-
-  # access_logs = {
-  #   bucket = "my-access-logs-bucket"
+  # Security Group
+  # security_group_ingress_rules = {
+  #   all_http = {
+  #     from_port   = 80
+  #     to_port     = 80
+  #     ip_protocol = "tcp"
+  #     description = "HTTP web traffic"
+  #     cidr_ipv4   = "0.0.0.0/0"
+  #   }
+  #   all_https = {
+  #     from_port   = 443
+  #     to_port     = 443
+  #     ip_protocol = "tcp"
+  #     description = "HTTPS web traffic"
+  #     cidr_ipv4   = "0.0.0.0/0"
+  #   }
+  # }
+  # security_group_egress_rules = {
+  #   all = {
+  #     ip_protocol = "-1"
+  #     cidr_ipv4   = "0.0.0.0/0"
+  #   }
   # }
 
-  // ELB attachments
-  number_of_instances = length(module.ec2-private)
-  instances           = [for ec2private in module.ec2-private: ec2private.id]
+  # access_logs = {
+  #   bucket = "my-alb-logs"
+  # }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    alb-tg-1 = {
+      name_prefix                       = "alb-tg-"
+      protocol                          = "HTTP"
+      port                              = 80
+      target_type                       = "instance"
+      deregistration_delay              = 10
+      load_balancing_cross_zone_enabled = false
+
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+
+      protocol_version = "HTTP1"
+      # target_id        = aws_instance.this.id
+      # port             = 80
+      tags = local.common_tags
+    }
+  }
 
   tags = local.common_tags
+}
+
+resource "aws_lb_target_group_attachment" "alb-tg-1" {
+  # k -> ec2 instance
+  # v -> ec2 instance details
+  for_each         = {
+    for k, v in module.ec2-private: k => v
+  }
+  target_group_arn = module.alb.target_groups["alb-tg-1"].arn
+  target_id        = each.value.id
+  port             = 80
 }
